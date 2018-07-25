aec922c7d7e8be550c16a8c8d758e92825acb156
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-25 03:12:33 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -4,7 +4,7 @@ array = [1, 2, 3, 4, 5]
 def my_each(array)
   counter = 0
   
-  while counter < 6
+  while counter < array.length
     "I'm number #{my_each[index+1]} in this array."
     counter += 1
   end
