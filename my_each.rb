593bc8b6bf14131abc1d76553e2383ab2bdfea41
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-25 03:14:04 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,9 +3,9 @@ array = [1, 2, 3, 4, 5]
 
 def my_each(array)
   counter = 0
-  while counter < array.length
-    yield(array[counter])
-    counter += 1
-  end
+    while counter < array.length
+      yield(array[counter])
+      counter += 1
+    end
   array
 end
